8e32676913ad438a12bece2b0ec9f4bdbfd98185
berkelpam <paul.tools@icloud.com>
2017-07-19 13:05:11 +0200

    First version
    Signed-off-by: berkelpam <paul.tools@icloud.com>

---
+++ Calculator/src/Caclulator.java
@@ -0,0 +1,44 @@
+
+public class Caclulator {
+private int firstnum;
+private int secondnum;
+
+public void reset() {
+	firstnum=1;
+	secondnum=1;
+}
+	
+public void setFirstnum(int firstnum) {
+	this.firstnum=firstnum;
+}
+
+public void setSecondnum(int secondnum) {
+	this.secondnum=secondnum;
+}
+	
+public int result() {
+	return (int) (firstnum-secondnum);
+}
+
+public int sum() {
+	return (int)(firstnum+secondnum);
+}
+
+public int subtract() {
+	return (int)(firstnum-secondnum);
+}
+
+public int multiply() {
+	return (int)(firstnum*secondnum);
+}
+
+public float divide() {
+	if (secondnum ==0) {
+		return (0);
+	}
+	else{
+		return(float)(firstnum/secondnum);
+	}
+}
+
+}
